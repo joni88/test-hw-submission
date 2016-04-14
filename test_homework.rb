9eba2d7602ea695f9d6eb0a74f299ddbc74c050f
Fatjon <joni_kerxhaliu@hotmail.com>
2016-04-13 21:19:11 -0400

    completing homework

--- test_homework.rb
+++ test_homework.rb
@@ -1,7 +1,7 @@
 # write a line of code that multiplies 3 * 5 and prints the result to the terminal
-
+ puts 3 * 8 
 
 # Use a string method to print out the length of your name
 
-
+puts "fatjon".length
 # Yet another update
